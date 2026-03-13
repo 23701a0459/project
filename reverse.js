76e2b9a7f7bdec701403046181eac8cda01e1c31
23701a0459 <gouthamilakkineni@gmail.com>
2026-03-13 11:55:35 +0530

    Add files via upload

---
+++ reverse.js
@@ -0,0 +1,9 @@
+function reverseList(arr) {
+  return arr.reverse();
+}
+
+// Example
+let fruits = ["apple", "banana", "mango", "orange"];
+let reversed = reverseList(fruits);
+
+console.log(reversed);
